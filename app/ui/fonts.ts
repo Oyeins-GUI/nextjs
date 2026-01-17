53a370d2c9d39bdaca7fa24f3c4fbcc7a9d56b10
Oyeins-GUI <oyeinx7@gmail.com>
2026-01-18 00:49:01 +0100

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+export const lusitana = Lusitana({
+   subsets: ["latin"],
+   variable: "--font-lusitana",
+   weight: ["400", "700"],
+});
