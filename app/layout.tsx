367aebee8cd365b1a348af28c6db60e185a94397
Oyeins-GUI <oyeinx7@gmail.com>
2026-01-21 14:59:01 +0100

    chore: app metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import "@/app/ui/global.css";
 import { inter } from "./ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+   title: {
+      template: "%s | Acme Dashboard",
+      default: "Acme Dashboard",
+   },
+   description: "The official Next.js Course Dashboard, built with App Router.",
+   metadataBase: new URL("http://nextjs-odashboard.vercel.app/"),
+   keywords: ["acme", "acme dashboard", "invoice"],
+};
 
 export default function RootLayout({
    children,
